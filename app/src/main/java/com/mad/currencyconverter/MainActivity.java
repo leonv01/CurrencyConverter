fc52879dfdd4d37eab9ff55dd5be2a099449d4b2
Leon <leonvogt2001@gmail.com>
2023-10-22 14:38:34 +0200

    added functional currency conversion and error prevention

--- app/src/main/java/com/mad/currencyconverter/MainActivity.java
+++ app/src/main/java/com/mad/currencyconverter/MainActivity.java
@@ -10,6 +10,11 @@ import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
     ExchangeRateDatabase database = new ExchangeRateDatabase();
     Spinner spinnerIn, spinnerOut;
@@ -34,8 +39,35 @@ public class MainActivity extends AppCompatActivity {
         valueOutResult = (TextView) findViewById(R.id.textValueResult);
 
         calculate = (Button) findViewById(R.id.buttonConvert);
-        calculate.setOnClickListener(ev -> {
-            valueOutResult.setText(valueIn.getText());
-        });
+        calculate.setOnClickListener(ev -> convert());
+    }
+
+    public void convert(){
+        // get current selected items by the spinners
+        String inputItem = (String) spinnerIn.getSelectedItem();
+        String outputItem = (String) spinnerOut.getSelectedItem();
+
+        // get exchange rate
+        double input;
+        try {
+            input = Double.parseDouble(valueIn.getText().toString());
+        }
+        catch (NumberFormatException e){
+            input = 0;
+        }
+        double fromValue = database.getExchangeRate(inputItem);
+        double toValue = database.getExchangeRate(outputItem);
+
+        // convert from current currency into Euro
+        input /= fromValue;
+
+        // convert from Euro to target currency
+        input *= toValue;
+
+        // format result into currency with fitting signings
+        NumberFormat format = NumberFormat.getCurrencyInstance();
+        format.setCurrency(Currency.getInstance(outputItem));
+        String str = format.format(input);
+        valueOutResult.setText(str);
     }
 }
